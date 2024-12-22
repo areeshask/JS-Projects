d4c1e3edf3a0d4ed32545b00f00df694c02cf4ee
ARISHA KHATRI <arishakhatri877@gmail.com>
2024-12-23 00:54:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 // setInterval allows real life changing of our time
 setInterval(() => {
     document.getElementById("time").textContent=new Date().toLocaleTimeString();
-}, 1000);
-
-//OR
-
-// let time = document.getElementById("time");
-// setInterval(() => {
-//     time.textContent=new Date().toLocaleTimeString();
-// },1000);
-
-  
-  
+}, 1000);  
